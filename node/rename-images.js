b1943263f83befce74d76f888ff81c47b12ae439
Sejal Patel <sejal@inigma.org>
2015-08-17 22:41:18 -0400

    Renaming images is now synchronous
    This prevents the name collisions from causing overrides when the
    concurrent threads happen to collide resulting in a duplicate exists
    check (theory) and thus a dual renaming of files to the same name
    losing one of the files as a result.

--- node/rename-images.js
+++ node/rename-images.js
@@ -1,61 +1,92 @@
-var fs = require('fs');
-var path = require('path');
-var exif = require('exif').ExifImage;
+var Q = require('q');
+var FS = require('fs');
+var Path = require('path');
+var Exif = require('exif').ExifImage;
 
 var args = process.argv.splice(2);
 if (args.length == 0) {
   args.push('.');
 }
 
-args.forEach(convertPath);
-
-function convertPath(pathname) {
-  pathname = path.normalize(pathname);
-  fs.stat(pathname, function(error, stat) {
-    if (error) {
-      console.info("Unable to process", pathname);
-      return;
-    }
-
-    if (stat.isDirectory()) {
-      fs.readdir(pathname, function(error, files) {
-        files.forEach(function(file) {
-          convertPath(pathname + '/' + file);
-        })
-      });
-    } else {
-      new exif({image: pathname}, function(error, exifData) {
-        if (error) {
-          console.error("Ignoring", pathname, error);
-          return;
-        }
-
-        var dateCreationString = exifData.exif.DateTimeOriginal || exifData.image.ModifyDate;
-        if (dateCreationString == null) {
-          console.info("Unable to handle renaming", pathname);
-          return;
-        }
-        var timestamp = dateCreationString.replace(/(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/, function(matcher, year, month, day, hour, minute, second) {
-          return year + "-" + month + "-" + day + " " + hour + "." + minute + "." + second;
-        });
-
-        if (pathname.indexOf(timestamp) != -1) { // already in the correct format. Don't rename it
-          return;
-        }
-
-        var newImageName = path.dirname(pathname) + '/' + timestamp + '.jpg';
-        if (fs.existsSync(newImageName)) {
-          for (var i = 0; i < 1000; i++) {
-            newImageName = path.dirname(pathname) + '/' + timestamp + '_' + i + '.jpg';
-            if (!fs.existsSync(newImageName)) {
-              break;
-            }
-          }
-        }
-
-        fs.renameSync(pathname, newImageName);
-      });
+var qList = [];
+args.forEach(function(pathname) {
+  convertPath(pathname).then(function(files) {
+    files.forEach(function(file) {
+      qList.push(renameImage(file));
+    });
+  });
+});
+Q.allSettled(qList).then(function(results) {
+  results.forEach(function(result) {
+    if (result.state == 'rejected') {
+      console.error(result.reason);
     }
   });
+}).done();
+
+function convertPath(pathname) {
+  var imagePaths = [];
+  pathname = Path.normalize(pathname);
+  var stat = FS.statSync(pathname);
+
+  if (stat.isDirectory()) {
+    FS.readdirSync(pathname).forEach(function(file) {
+      // convertPath is synchronous not asynchronous
+      convertPath(pathname + '/' + file).then(function(imageFiles) {
+        imageFiles.forEach(function(image) {
+          imagePaths.push(image);
+        });
+      });
+    });
+  } else {
+    imagePaths.push(pathname);
+  }
+
+  return Q(imagePaths);
+}
+
+function renameImage(filename) {
+  if (!filename.match(/\.jpg/i)) {
+    return Q.reject({filename: filename, error: "Not a .jpg extension"});
+  }
+
+  var defer = Q.defer();
+  new Exif({image: filename}, function(error, exifData) {
+    if (error) {
+      return defer.reject({filename: filename, error: error});
+    }
+
+    var dateCreationString = exifData.exif.DateTimeOriginal || exifData.image.ModifyDate;
+    if (dateCreationString == null) {
+      return defer.reject({filename: filename, error: "Unable to handle renaming"});
+    }
+    var timestamp = dateCreationString.replace(/(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)/, function(matcher, year, month, day, hour, minute, second) {
+      return year + "-" + month + "-" + day + " " + hour + "." + minute + "." + second;
+    });
+
+    if (filename.indexOf(timestamp) != -1) { // already in the correct format. Don't rename it
+      return defer.reject({filename: filename, error: "Already in the correct format"});
+    }
+
+    var newImageName = Path.dirname(filename) + '/' + timestamp + '.jpg';
+    if (FS.existsSync(newImageName)) {
+      for (var i = 0; i < 1000; i++) {
+        newImageName = Path.dirname(filename) + '/' + timestamp + '_' + i + '.jpg';
+        if (!FS.existsSync(newImageName)) {
+          break;
+        }
+      }
+    }
+
+    try {
+      console.info("Renaming", filename, "to", newImageName);
+      FS.renameSync(filename, newImageName);
+      defer.resolve({original: filename, destination: newImageName});
+    } catch (e) {
+      defer.reject({filename: filename, error: e});
+    }
+  });
+
+  return defer.promise;
 }
 
